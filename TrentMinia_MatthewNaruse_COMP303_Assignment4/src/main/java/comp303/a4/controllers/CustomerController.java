ea5b997588c4d41ed683335673bdf4e223155781
Matt Naruse <matnaruse@gmail.com>
2020-12-03 16:39:33 -0500

    [MN8] Minor "register.html" progress

--- TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/controllers/CustomerController.java
+++ TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/controllers/CustomerController.java
@@ -8,9 +8,39 @@
 
 package comp303.a4.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+
+import comp303.a4.entities.Customer;
+import comp303.a4.repositories.CustomerRepo;
+
 
 @Controller
 public class CustomerController {
 
+	@Autowired
+	private CustomerRepo custRepo;
+	
+	@GetMapping("/register")
+	public String newRegisterForm() {
+		return "register";
+	}
+	
+	@PostMapping("/new_customer")
+	public ModelAndView registerNewCustomer(@RequestParam("Username") String user, 
+											@RequestParam("Password") String pass,
+											@RequestParam("custName") String custName,
+											@RequestParam("address") String address,
+											@RequestParam("city") String city,
+											@RequestParam("email") String email,
+											@RequestParam("phoneNumber") String phone) {
+		
+		Customer customer = new Customer(user, pass, custName, address, city, email, phone);
+		custRepo.save(customer);
+		return new ModelAndView("index");
+	}
 }
--- TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/entities/Customer.java
+++ TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/entities/Customer.java
@@ -9,12 +9,23 @@
 package comp303.a4.entities;
 
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
 
 @Entity
 public class Customer {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int custId;
+	
+	@NotBlank(message="Must pick a User Name")
+	private String Username;
+	
+	@NotBlank(message="Must have a Password")
+	private String Password;
+	
 	private String custName;
 	private String address;
 	private String city;
@@ -26,6 +37,18 @@ public class Customer {
 	}
 	public void setCustId(int custId) {
 		this.custId = custId;
+	}
+	public String getUsername() {
+		return Username;
+	}
+	public void setUsername(String username) {
+		Username = username;
+	}
+	public String getPassword() {
+		return Password;
+	}
+	public void setPassword(String password) {
+		Password = password;
 	}
 	public String getCustName() {
 		return custName;
@@ -58,4 +81,14 @@ public class Customer {
 		this.phoneNumber = phoneNumber;
 	}
 	
+	
+	public Customer(String user, String pass, String name, String address, String city, String email, String phone) {
+		this.Username = user;
+		this.Password = pass;
+		this.custName = name;
+		this.address = address;
+		this.city = city;
+		this.email = email;
+		this.phoneNumber = phone;
+	}
 }
