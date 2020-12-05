ba242eaade33f857ef794613b9c2dfe9f5700828
Trent B Minia <trentbmin@gmail.com>
2020-12-04 21:29:43 -0500

    [TM1-1] Setting up SeedData for movies
    * Movie class updated with more properties
    * SeedData class created

--- TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/entities/Movie.java
+++ TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/entities/Movie.java
@@ -18,7 +18,10 @@ public class Movie {
 	private String movieName;
 	private String movieLanguage;
 	private String movieGenre;
+	private String movieBlurb;
+	private String movieRating; // movie rating for appropriate audiences
 	private int duration;
+	private String imgSrc;
 	
 	public int getMovieId() {
 		return movieId;
@@ -44,17 +47,44 @@ public class Movie {
 	public void setMovieGenre(String movieGenre) {
 		this.movieGenre = movieGenre;
 	}
+	public String getMovieBlurb() {
+		return movieBlurb;
+	}
+	public void setMovieBlurb(String movieBlurb) {
+		this.movieBlurb = movieBlurb;
+	}
+	public String getMovieRating() {
+		return movieRating;
+	}
+	public void setMovieRating(String movieRating) {
+		this.movieRating = movieRating;
+	}
 	public int getDuration() {
 		return duration;
 	}
 	public void setDuration(int duration) {
 		this.duration = duration;
 	}
+	public String getImgSrc() {
+		return imgSrc;
+	}
+	public void setImgSrc(String imgSrc) {
+		this.imgSrc = imgSrc;
+	}
 	
-	public Movie(String name, String lang, String genr, int dur) {
+	public Movie(String name, 
+				 String lang, 
+				 String genr,
+				 String blur,
+				 String rat,
+				 int dur,
+				 String img) {
 		this.movieName = name;
 		this.movieLanguage = lang;
 		this.movieGenre = genr;
+		this.movieBlurb = blur;
+		this.movieRating = rat;
 		this.duration = dur;
+		this.imgSrc = img;
 	}
 }
