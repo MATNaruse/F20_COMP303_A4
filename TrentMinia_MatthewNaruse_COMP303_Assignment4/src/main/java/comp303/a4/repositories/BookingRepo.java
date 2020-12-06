e454af57e5c52c907e938410fe96e4f672ca258f
Matt Naruse <matnaruse@gmail.com>
2020-12-06 16:14:17 -0500

    [MN6-1] Added List of Bookings on Profile Page
    AND the links to view bookings work!

--- TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/repositories/BookingRepo.java
+++ TrentMinia_MatthewNaruse_COMP303_Assignment4/src/main/java/comp303/a4/repositories/BookingRepo.java
@@ -1,9 +1,15 @@
 package comp303.a4.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import comp303.a4.entities.Booking;
+import comp303.a4.entities.Customer;
 
 public interface BookingRepo extends JpaRepository<Booking, Integer>{
-
+	
+	@Query(value="SELECT b FROM Booking b WHERE b.custId LIKE (:custId)")
+	List<Booking> findAllBookingsByCustId(int custId);
 }
